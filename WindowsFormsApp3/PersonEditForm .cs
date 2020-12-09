8bf1e10ed031c92c0860d5832a511ebbe7cc07e1
Alexei1488 <alextarase@gmail.com>
2020-12-09 23:19:17 +0700

    comment added

--- WindowsFormsApp3/PersonEditForm .cs
+++ WindowsFormsApp3/PersonEditForm .cs
@@ -24,7 +24,11 @@ namespace WindowsFormsApp3
         {
             Close();
         }
-
+        /// <summary>
+        /// button 1 click function
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
           
